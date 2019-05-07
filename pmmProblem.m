114fba39ff55a9e37659b2f51b5f7761c1e490c1
Jesus Mejia <jesusmejded@gmail.com>
2019-05-07 18:38:49 -0500

    improve testing

--- pmmProblem.m
+++ pmmProblem.m
@@ -14,17 +14,17 @@ fnum_global = fnum;
 
 ulPopSize=100;                    % Size of UL population
 ulMaxGens=1000;                  % Maximum number of generations allowed at UL
-ulDim=5;                         % Number of UL dimensions
+ulDim=2;                         % Number of UL dimensions
 
 llPopSize=100;                    % Size of LL population
 llMaxGens=1000;                  % Maximum number of generations allowed at LL
-llDim=5;                         % Number of LL dimensions
+llDim=3;                         % Number of LL dimensions
 
 ulDimMin = -10*ones(1, ulDim);            % Minimum value accross UL dimensions
 ulDimMax = 10*ones(1, ulDim);              % Maximum value accross UL dimensions
 
-llDimMin = ulDimMin;      % Minimum value accross LL dimensions
-llDimMax = ulDimMax;     % Maximum value accross LL dimensions
+llDimMin = -10*ones(1, llDim);      % Minimum value accross LL dimensions
+llDimMax = 10*ones(1, llDim);     % Maximum value accross LL dimensions
 
 ulStoppingCriteria = 1e-5;
 llStoppingCriteria = 1e-5;
